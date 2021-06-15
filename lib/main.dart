7ff5640970f8be574482abbce31d87dfda7798b0
Hritthik Roy <royhritthik@gmail.com>
2021-06-15 19:15:06 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Myapp());
+}
+
+
+class Myapp extends StatelessWidget {
+  const Myapp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+              child: Center(
+                child: Container(
+          child: Text("My name is hritthik"),
+        ),
+              ),
+      ),
+    );
+  }
+}  
